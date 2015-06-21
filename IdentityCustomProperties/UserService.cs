64d82f3bc5ce76992d537580cf3141b9d0dbb432
bananagun <bananagun@live.com>
2015-06-21 14:29:13 -0400

    Add identity custom properties classes to link to from blog.

---
+++ IdentityCustomProperties/UserService.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Principal;
+using System.Web;
+using EF.Implementation;
+using EntityManager.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace EntityManager.Services
+{
+    public interface IUserService
+    {
+        string GetUserProperty(string property);
+    }
+
+    public class UserService : IUserService
+    {
+        public DbContextScopeFactory DbContextScopeFactory;
+        private readonly IPrincipal _user;
+
+        public UserService(DbContextScopeFactory dbContextScopeFactory)
+        {
+            DbContextScopeFactory = dbContextScopeFactory;
+            _user = HttpContext.Current.User;
+        
+        }
+
+        public string GetUserProperty(string property)
+        {
+            using (var dbContextScope = DbContextScopeFactory.CreateReadOnly())
+            {
+                var context = dbContextScope.DbContexts.Get<ApplicationDbContext>();
+
+                var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+
+                var currentUser = manager.FindById(_user.Identity.GetUserId());
+                return currentUser.GetType().GetProperty(property).GetValue(currentUser, null).ToString();
+            }
+        }
+    }
+}
