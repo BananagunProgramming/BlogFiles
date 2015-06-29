6f58c8518007d19695f74a491857d98060948eda
bananagun <bananagun@live.com>
2015-06-29 11:47:15 -0400

    Azure Service Bus samples

---
+++ ServiceBus/RecieveFromQueue/RecieveFromQueue/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ReceiveFromQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBusRecieve();
+            //TopicReceive(2);
+            //EventHubReceive();
+        }
+
+        private static void EventHubReceive()
+        {
+            var ehName = "davnaeventhub";
+            var connection = "Endpoint=sb://alewife.servicebus.windows.net/;SharedAccessKeyName=Receiver;SharedAccessKey=3jaTeykwRu84x93OpW3DftRO1WSHt4oi7QB0FRyHoyg=;TransportType=Amqp";
+            
+            MessagingFactory factory = MessagingFactory.CreateFromConnectionString(connection);
+
+            EventHubClient ehub = factory.CreateEventHubClient(ehName);
+            EventHubConsumerGroup group = ehub.GetDefaultConsumerGroup();
+            EventHubReceiver receiver = group.CreateReceiver("1");
+
+            while (true)
+            {
+                EventData data = receiver.Receive();
+
+                if (data != null)
+                {
+                    var message = Encoding.UTF8.GetString(data.GetBytes());
+
+                    Console.WriteLine("PartitionKey: {0}", data.PartitionKey);
+                    Console.WriteLine("SequenceNumber: {0}", data.SequenceNumber);
+                    Console.WriteLine(message);
+                }
+            }
+        }
+
+        private static void TopicReceive(int subscription)
+        {
+            var topicName = "topicdemo";
+            var subToProcess = "Subscription" + subscription;
+            var connection = "Endpoint=sb://alewife.servicebus.windows.net/;SharedAccessKeyName=Receiver;SharedAccessKey=Vqa3SwP4qH5/+8ikB4B/BNMPQCRDuV/AiUYsUofWebo=;TransportType=Amqp";
+
+            MessagingFactory factory = MessagingFactory.CreateFromConnectionString(connection);
+            SubscriptionClient clientA = factory.CreateSubscriptionClient(topicName, subToProcess);
+
+            while (true)
+            {
+                BrokeredMessage message = clientA.Receive();
+                if (message != null)
+                {
+                    try
+                    {
+                        Console.WriteLine("MessageId:{0}", message.MessageId);
+                        Console.WriteLine(message.GetBody<string>());
+                        message.Complete();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        message.Abandon();
+                    }
+                }
+            }
+        }
+
+        private static void ServiceBusRecieve()
+        {
+            string qName = "76BusQueue";
+            string connection =
+                "Endpoint=sb://alewife.servicebus.windows.net/;SharedAccessKeyName=76BusReceiver;SharedAccessKey=/ot1r8HCUNnLScKABbOHharCzOBQqoNtvbxlfVKPyRQ=;TransportType=Amqp";
+
+            MessagingFactory factory = MessagingFactory.CreateFromConnectionString(connection);
+
+            QueueClient queue = factory.CreateQueueClient(qName);
+
+            var counter = 1;
+            while (true)
+            {
+                BrokeredMessage bm = queue.Receive();
+
+                if (bm != null)
+                {
+                    try
+                    {
+                        Console.WriteLine("MessageId {0}", bm.MessageId);
+                        Console.WriteLine(counter);
+                        Console.WriteLine(bm.GetBody<string>());
+                        bm.Complete();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        bm.Abandon();
+                    }
+                }
+                counter++;
+            }
+        }
+    }
+}
---
+++ ServiceBus/SendToQueue/SendToQueue/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace SendToQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBusSend();
+            //ServiceTopicSend();
+            //SendToEventHub();
+        }
+
+        private static void SendToEventHub()
+        {
+            var ehName = "davnaeventhub";
+            var connection = "Endpoint=sb://<yournamespacehere>.servicebus.windows.net/;SharedAccessKeyName=Sender;SharedAccessKey=<yourkeyhere>;TransportType=Amqp";
+
+            MessagingFactory factory = MessagingFactory.CreateFromConnectionString(connection);
+
+            EventHubClient client = factory.CreateEventHubClient(ehName);
+
+            for (var i = 0; i < 10000; i++)
+            {
+                var message = i + " event hub message";
+
+                EventData data = new EventData(Encoding.UTF8.GetBytes(message));
+
+                client.Send(data);
+   
+                Console.WriteLine(message);
+            }
+        }
+
+        private static void ServiceTopicSend()
+        {
+            var topicName = "topicdemo";
+            var connection = "Endpoint=sb://<yournamespacehere>.servicebus.windows.net/;SharedAccessKeyName=Sender;SharedAccessKey=<yourkeyhere>;TransportType=Amqp";
+
+            MessagingFactory factory = MessagingFactory.CreateFromConnectionString(connection);
+
+            TopicClient topic = factory.CreateTopicClient(topicName);
+
+            for (var i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+                topic.Send(new BrokeredMessage("topic message" + i));
+            }
+        }
+
+        private static void ServiceBusSend()
+        {
+            var qName = "76BusQueue";
+            var connection = "Endpoint=sb://<yournamespacehere>.servicebus.windows.net/;SharedAccessKeyName=76BusSender;SharedAccessKey=<yourkeyhere>;TransportType=Amqp";
+
+            MessagingFactory factory = MessagingFactory.CreateFromConnectionString(connection);
+
+            QueueClient queue = factory.CreateQueueClient(qName);
+
+            for (var i = 0; i < 100; i++)
+            {
+                string message = "Thurman Thomas is old -" + DateTime.UtcNow.Ticks;
+                BrokeredMessage bm = new BrokeredMessage(message);
+
+                queue.Send(bm);
+
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
